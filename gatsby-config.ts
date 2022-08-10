4821f2d12cc9b9c166915f165bf407fb77dc0294
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2022-08-10 14:40:31 -0700

    Chore: Remove eslint gatsby plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -46,28 +46,6 @@ const config: GatsbyConfig = {
         isUsingColorMode: true,
       },
     },
-    {
-      resolve: "gatsby-plugin-prettier-eslint",
-      options: {
-        prettier: {
-          patterns: [
-            "**/*.{css,scss,less}",
-            "**/*.{json,json5}",
-            "**/*.{graphql}",
-            "**/*.{md,mdx}",
-            "**/*.{html}",
-            "**/*.{yaml,yml}",
-          ],
-        },
-        eslint: {
-          patterns: "**/*.{js,jsx,ts,tsx}",
-          customOptions: {
-            fix: true,
-            cache: true,
-          },
-        },
-      },
-    },
   ],
 };
 
